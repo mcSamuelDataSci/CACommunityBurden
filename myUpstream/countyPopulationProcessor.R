6d16c3b443114d1df1f54aab9e31cf6295c41216
mcSamuelDataSci <samfam921@gmail.com>
2018-07-13 14:50:10 -0700

    -small pop processor changes

--- myUpstream/countyPopulationProcessor.R
+++ myUpstream/countyPopulationProcessor.R
@@ -1,5 +1,5 @@
 # ====================================================================================================
-# "countyPopulationProcessor.R" file                                                              |
+# "countyPopulationProcessor.R" file                                                                 |
 #                                                                                                    |
 #            Reads in age-race-sex-1980-2015 text file provided by CDPH-CID-DCDC-STDCB               |
 #            Subsets and processes data, and saves as County by year total population file           |
@@ -21,7 +21,7 @@ library(readxl)
 source(paste0(myPlace,"/myFunctions/helperFunctions/capwords.R"))
 
 # special file location becuase file is too big for GitHub
-# tDat        <- read.delim(file=paste0(myDrive,"/0.CBD/Resources/populationData/1980_2025.txt"),sep="\t", header = TRUE, stringsAsFactors = FALSE)
+# tDat        <- read.delim(file=paste0(myDrive,"/0.CBD.Other/Resources/populationData/1980_2025.txt"),sep="\t", header = TRUE, stringsAsFactors = FALSE)
 # tDat        <- filter(tDat, YEAR >=2000 & YEAR <= 2020)
 # saveRDS(tDat, file= paste0(upPlace,"/upData/tDat_2000_2020.rds"))
 tDat <- readRDS(file= paste0(upPlace,"/upData/tDat_2000_2020.rds"))
@@ -36,7 +36,6 @@ names(tDat) <- c("county","year","sex","age","raceE","OrigPop","pop" )
 # tDat        <- filter(tDat, year %in% 2000:2015 & !(county %in% c("Alameda HD","Berkeley","Pasadena","Long Beach","Los Angeles HD")))
 
 
-
 tDat        <- filter(tDat, year %in% 2000:2015 & !(county %in% c("Alameda HD","Berkeley","Pasadena","Long Beach","Los Angeles HD")))
 
 library(readr)
@@ -46,7 +45,6 @@ library(readr)
 tDatDOF        <- read_csv("https://data.ca.gov/sites/default/files/dof_dru_pop_1970_2050_csya_wide.csv",col_types="_cii__i")
 tDatDOF        <- filter(tDatDOF, year %in% 2000:2015)
 tDatDOF$county <- capwords(tDatDOF$county,strict=TRUE)
-
 
 
 # -- Make and save file with County (and California Total) Pop by year 2000-2015 -----------------------------------------------------------
