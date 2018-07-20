81e23ecf6284bfbea3b2575b26bc1071f519daf0
benjamin-hicks-cdph-ca-gov <41485729+benjamin-hicks-cdph-ca-gov@users.noreply.github.com>
2018-07-20 11:41:12 -0700

    Added ACS table variables B02001 and B01001 series. Added code to pull and merge ACS labels and append them to table (as "variable") without .csv label file.

--- myUpstream/tractPopulationProcessorTotal.R
+++ myUpstream/tractPopulationProcessorTotal.R
@@ -24,17 +24,32 @@ myDrive  <- "E:"
 myPlace  <- path(myDrive,"/0.CBD/myCBD///")  
 upPlace  <- path(myDrive,"/0.CBD/myUpstream")  
 
+#-- Set API ACS Table/variable list ----------------------------------------------------------------------
+# B01001_*E except totals   MCS FIXED FROM ---- c(3:26,28:49))
+
+acs.varlist <- "B01003_001"                                     # B01003_001       total: census tract x year
+# acs.varlist <- sprintf("B02001_%03d",c(1:10)) 		              # B02001_001-010   total: census tract x year x race (H incl)
+# acs.varlist <- c("B01001_001E","B01001_002E", "B01001_026E")    # B01001_002E,026E total: census tract x year x sex (Total, Male, Female)
+
 #-- Get, process, and export data -----------------------------------------------------------------------------------
 
 options(tigris_use_cache = TRUE)
-popTractTot2013   <- get_acs(geography = "tract", variables = "B01003_001", state = "CA",year=2013)  # GEOID is 11 digit character string - 2011-2015 - N=8057
+popTractTot2013   <- get_acs(geography = "tract", variables = acs.varlist, state = "CA",year=2013)  # GEOID is 11 digit character string - 2011-2015 - N=8057
+
+.acsvariables <- load_variables(2013,"acs5")
+mylabels<-.acsvariables
+names(mylabels)[names(mylabels)== "name"] = "variable"                                                        # rename variable to Name (to merge with labels)
+mylabels<-mylabels[ (substr(mylabels$variable,0,7)==substring(acs.varlist,0,7)) &
+                    (substr(mylabels$variable,nchar(mylabels$variable),nchar(mylabels$variable)) == "E"), ]   # filter mylabels to contain only entries from acs.varlist
+mylabels$variable<-substr(mylabels$variable,1,nchar(mylabels$variable)-1)
+popTractTot2013<-merge(popTractTot2013,mylabels)
 
 cbdLinkCA         <- read.csv(path(myPlace,"/myInfo/cbdLinkCA.csv"),colClasses = "character") # maps census tracts to MSSAs and counties - N= 8036
 popTractTot2013   <- popTractTot2013 %>% mutate(yearG = "2011-2015",  
                                                 comID  = cbdLinkCA[match(popTractTot2013$GEOID,cbdLinkCA[,"GEOID"]),"comID"],
                                                 county = cbdLinkCA[match(popTractTot2013$GEOID,cbdLinkCA[,"GEOID"]),"county"],
                                                 pop    = estimate              ) %>%
-                                         select(-NAME,-estimate,-variable)
+                                         select(-NAME,-estimate,-variable,-concept)
 
 saveRDS(popTractTot2013, file=paste0(upPlace,"/upData/popTractTot2013.RDS"))
 
