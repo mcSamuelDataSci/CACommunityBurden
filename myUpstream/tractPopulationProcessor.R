50d85a061c8e886900d5180e2da151b969e4119b
mcSamuelDataSci <samfam921@gmail.com>
2018-07-23 22:11:19 -0700

    -minor change to tract pop make

--- myUpstream/tractPopulationProcessor.R
+++ myUpstream/tractPopulationProcessor.R
@@ -34,6 +34,7 @@ list.sex  <- c("B01001_001E","B01001_002E", "B01001_026E")    # B01001_002E,026E
 
 #-- Get, process, and export data -----------------------------------------------------------------------------------
 
+# make year an iinput...?
 
 makePop <- function(inList) {
 
@@ -43,24 +44,25 @@ options(tigris_use_cache = TRUE)
 workDat   <- get_acs(geography = "tract", variables = acs.varlist, state = "CA",year=2013)  # GEOID is 11 digit character string - 2011-2015 - N=8057
 
 .acsvariables <- load_variables(2013,"acs5")
-mylabels<-.acsvariables
-names(mylabels)[names(mylabels)== "name"] = "variable"                                                        # rename variable to Name (to merge with labels)
+mylabels      <- .acsvariables
+names(mylabels)[names(mylabels) == "name"] = "variable"                                                       # rename variable to Name (to merge with labels)
 mylabels<-mylabels[ (substr(mylabels$variable,0,7)==substring(acs.varlist,0,7)) &
                     (substr(mylabels$variable,nchar(mylabels$variable),nchar(mylabels$variable)) == "E"), ]   # filter mylabels to contain only entries from acs.varlist
-mylabels$variable<-substr(mylabels$variable,1,nchar(mylabels$variable)-1)
-workDat<-merge(workDat,mylabels)
+mylabels$variable <- substr(mylabels$variable,1,nchar(mylabels$variable)-1)
+workDat <- merge(workDat,mylabels)
 
-cbdLinkCA         <- read.csv(path(myPlace,"/myInfo/cbdLinkCA.csv"),colClasses = "character") # maps census tracts to MSSAs and counties - N= 8036
+cbdLinkCA <- read.csv(path(myPlace,"/myInfo/cbdLinkCA.csv"),colClasses = "character") # maps census tracts to MSSAs and counties - N= 8036
 workDat   <- workDat %>% mutate(yearG = "2011-2015",  
-                                                comID  = cbdLinkCA[match(workDat$GEOID,cbdLinkCA[,"GEOID"]),"comID"],
-                                                county = cbdLinkCA[match(workDat$GEOID,cbdLinkCA[,"GEOID"]),"county"],
-                                                pop    = estimate              ) %>%
-                                         select(-NAME,-estimate,-variable,-concept)
+                                comID  = cbdLinkCA[match(workDat$GEOID,cbdLinkCA[,"GEOID"]),"comID"],
+                                county = cbdLinkCA[match(workDat$GEOID,cbdLinkCA[,"GEOID"]),"county"],
+                                pop    = estimate              ) %>%
+                         select(-NAME,-estimate,-variable,-concept)
 
 }
 
 tDat <- makePop(list.sex)                                                   # edit "sex" or "race" as appropate
-tDat <- tDat %>%  filter(label !="Total:") %>%
+tDat <- tDat %>%  
+                # filter(label !="Total:") %>%
                   transform(label = str_replace(label, ":", "")) %>%
                   transform(sex   = label) %>%                              # edit "sex" or "race" as appropate
                   select(-label)  %>%
@@ -68,15 +70,9 @@ tDat <- tDat %>%  filter(label !="Total:") %>%
 saveRDS(tDat, file=paste0(upPlace,"/upData/popTractSex2013.RDS"))           # edit "sex" or "race" as appropate
 
 
-tDat <- makePop(list.tot) 
-tDat <- tDat %>%  select(-label)  
-saveRDS(tDat, file=paste0(upPlace,"/upData/popTractTot2013.RDS"))          
-
-
-
-
-
-#saveRDS(popTractTot2013, file=paste0(upPlace,"/upData/popTractTot2013.RDS"))
+# tDat <- makePop(list.tot) 
+# tDat <- tDat %>%  select(-label)  
+# saveRDS(tDat, file=paste0(upPlace,"/upData/popTractTot2013.RDS"))          
 
 # Ben: this didn't work for the race file, so I changed it to above transform
 #                  transform(label = substr(label,1,nchar(label)-1)) %>%
