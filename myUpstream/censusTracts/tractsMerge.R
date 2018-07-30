30554537038c9169a90b4ad89091a06fd9c97b89
mcSamuelDataSci <samfam921@gmail.com>
2018-07-30 14:16:34 -0700

    -Census tract merge file changed

--- myUpstream/censusTracts/tractsMerge.R
+++ myUpstream/censusTracts/tractsMerge.R
@@ -1,24 +1,24 @@
 
-
-infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\Deaths_County_Corrected.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
-infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\mssa00.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
-infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\mssa13.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
-infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\pov_2006_10.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
-infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\SVI_CDC_group_living.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
-infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\tracts_tiger.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
-
-
-
-data merged;
-merge deaths(in=a) HCI(in=b) group(in=c) shape(in=d) mssa2000(in=e) mssa2013(in=f);
-by GEOID county;
-if a then inDeaths=1;
-if b then inHCI=1;
-if c then inSVI=1;
-if d then inShapeX=1;
-if e then inCensus2000=1;
-if f then inCensus2010=1; 
-run;
+# 
+# infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\Deaths_County_Corrected.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
+# infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\mssa00.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
+# infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\mssa13.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
+# infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\pov_2006_10.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
+# infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\SVI_CDC_group_living.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
+# infile '\\pheecisilon00.file.cdphintra.ca.gov\exec\CCB\CensusTracts\Data_to_merge\tracts_tiger.csv' delimiter = ',' MISSOVER DSD lrecl=32767 firstobs=2 ;
+# 
+# 
+# 
+# data merged;
+# merge deaths(in=a) HCI(in=b) group(in=c) shape(in=d) mssa2000(in=e) mssa2013(in=f);
+# by GEOID county;
+# if a then inDeaths=1;
+# if b then inHCI=1;
+# if c then inSVI=1;
+# if d then inShapeX=1;
+# if e then inCensus2000=1;
+# if f then inCensus2010=1; 
+# run;
 
 library(fs)
 library(readr)
@@ -28,9 +28,14 @@ myDrive  <- "e:"
 myPlace  <- paste0(myDrive,"/0.CBD/myUpstream")  
 
 d.correct <- read_csv(path(myPlace,"censusTracts/myData","Deaths_County_Corrected.csv"),col_types = "ccnnn")
+d.mssa00  <- read_csv(path(myPlace,"censusTracts/myData","mssa00.csv")) %>%
+                select(GEOID,COUNTY,POP2000)
 
 
 #make geoid 11 digist with 0
 d.raw      <- read_csv(path(myPlace,"censusTracts/myData","rawDeaths.csv"),col_types = "ncnc") %>%
                 group_by(GEOID,county) %>%
                 summarize(n=sum(Ndeaths))
+
+
+
