8bedec20fe33d4b637422d383255f05c0cb17f3c
mcSamuelDataSci <samfam921@gmail.com>
2018-09-13 21:34:26 -0700

    misc

--- myUpstream/makeDatasets.R
+++ myUpstream/makeDatasets.R
@@ -398,14 +398,35 @@ write.csv(datComm,(paste0(upPlace,"/tempOutput/Community CCB Work.csv")))
 write.csv(datCounty,(paste0(upPlace,"/tempOutput/County CCB Work.csv")))
 write.csv(datState,(paste0(upPlace,"/tempOutput/State CCB Work.csv")))
 
-
-
-
 saveRDS(datTract,  file= path(myPlace,"/myData/",whichDat,"datTract.RDS"))
 saveRDS(datComm,   file= path(myPlace,"/myData/",whichDat,"datComm.RDS"))
 saveRDS(datCounty, file= path(myPlace,"/myData/",whichDat,"datCounty.RDS"))
 
 # END ===================================================================================================================
+
+# https://github.com/cran/epitools/blob/master/R/ageadjust.direct.R
+
+
+ageadjust.direct2 <- function (count, pop, rate = NULL, stdpop, conf.level = 0.95) 
+  {
+    if (missing(count) == TRUE & !missing(pop) == TRUE & is.null(rate) == TRUE)   count <- rate * pop
+    if (missing(pop) == TRUE & !missing(count) == TRUE & is.null(rate) == TRUE)     pop <- count/rate
+    if (is.null(rate) == TRUE & !missing(count) == TRUE & !missing(pop) == TRUE)  rate <- count/pop
+    alpha <- 1 - conf.level
+    cruderate <- sum(count)/sum(pop)
+    stdwt <- stdpop/sum(stdpop)
+    dsr <- sum(stdwt * rate)
+    dsr.var <- sum((stdwt^2) * (count/pop^2))
+    wm<- max(stdwt/pop)
+    gamma.lci <- qgamma(alpha/2, shape = (dsr^2)/dsr.var, scale = dsr.var/dsr)
+    gamma.uci <- qgamma(1 - alpha/2, shape = ((dsr+wm)^2)/(dsr.var+wm^2), 
+                        scale = (dsr.var+wm^2)/(dsr+wm))
+    c(crude.rate = cruderate, adj.rate = dsr, lci = gamma.lci, 
+      uci = gamma.uci)
+  }
+
+
+
 
 
  # DATA CLEANING ISSUES ----------------------------------
