2640d67d3051614b8533b97c43e3d4cf790b2785
Michael Samuel <samfam921@gmail.com>
2018-09-20 22:07:30 -0700

    Both sex map (#40)
    * junk
    
    * both sex bar chart
    
    * order

---
+++ myCBD/myFunctions/rankCausesBothSex.R
@@ -0,0 +1,54 @@
+rankCause  <- function(myLHJ="CALIFORNIA",myMeasure = "YLL",myYear=2015,mySex="Total",myN=10) {
+
+  myPlace   <- getwd()  
+  whichData <-  "real"
+  
+  
+  myLHJ = "CALIFORNIA"
+  myN = 20
+  myMeasure = "YLL"
+  myMeasure = "aRate"
+  myYear = 2015
+  
+  
+  library(fs)
+  library(ggplot2)
+  library(dplyr)
+  
+  datCounty <- readRDS(path("e:","0.CBD/myCBD/","/myData/",whichData,"datCounty.RDS"))
+  
+  
+  myCex <- 1.6
+  myCol <- "blue"            #mycol <- rep("blue",nrow(dat.1))
+
+  
+  inDat <- datCounty
+  dat.1 <- filter(inDat,county==myLHJ,year==myYear,sex != "Total",CAUSE !=0)
+  
+   dat.1 <- dat.1[order( eval(parse(text=paste0("dat.1$",myMeasure)))),]
+
+   if (myMeasure=="mean.age"){
+     dat.1 <- dat.1[order( eval(parse(text=paste0("dat.1$",myMeasure))),decreasing=TRUE),]}
+  
+  nR    <- nrow(dat.1)
+  myNX  <- min(nR,myN) 
+  dat.1 <- dat.1[((nR-myNX):nR),]
+  
+  dat.1$info <- dat.1[,myMeasure]
+  
+
+ # https://drsimonj.svbtle.com/ordering-categories-within-ggplot2-facets
+  
+  
+  
+  dat.1$CAUSE <- factor(dat.1$CAUSE, levels = dat.1$CAUSE[order(dat.1$info)])
+  
+  g <- ggplot(dat.1, aes(CAUSE,info))
+  g + geom_col() +  coord_flip() +
+      facet_grid(. ~ sex)
+  
+  
+}
+  
+  
+  
