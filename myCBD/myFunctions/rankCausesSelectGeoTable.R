88bd80674937b8a383d7aeb6f374455e287a513d
mcSamuelDataSci <samfam921@gmail.com>
2018-10-22 23:16:42 -0700

    data supression work

--- myCBD/myFunctions/rankCausesSelectGeoTable.R
+++ myCBD/myFunctions/rankCausesSelectGeoTable.R
@@ -1,13 +1,25 @@
 rankCauseTab  <- function(myLHJ="Alameda",myYear=2015,mySex="Total") {
+ 
+  if(1==2){
+    myYear <- 2017
+    myLHJ  <- "CALIFORNIA"
+    temp <- full_join(datCounty,causeList36,by=c("CAUSE"="LABEL"))
+    temp <- filter(temp,is.na(causeList))
+  }
+  
+  
+  
   mySex <- "Total"
   inDat <- datCounty
+  
+  
   dat.1 <- filter(inDat,county==myLHJ,year==myYear,sex==mySex,CAUSE !=0)
 
   dat.1$causeList <- causeList36[match(dat.1$CAUSE,causeList36[,"LABEL"]),"causeList"]
   
   dat.1 <- dat.1[,c("causeList","Ndeaths","cDeathRate","aRate","YLL","YLLper","YLL.adj.rate","SMR")]
   
-  dat.1[,3:6] <- round(dat.1[3:6],1)
+  dat.1[,3:7] <- round(dat.1[3:7],1)
   
  # mtext(paste("Measures by Cause,",myYear,myLHJ),outer = TRUE,cex=1.3,line=1)
 
