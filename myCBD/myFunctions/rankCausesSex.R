b8d6396103855ede346676ffd6ed2fdbe37da90f
Hicks <Benjamin.Hicks@cdph.ca.gov>
2018-11-19 15:52:02 -0800

    update to rankCausesSex

--- myCBD/myFunctions/rankCausesSex.R
+++ myCBD/myFunctions/rankCausesSex.R
@@ -1,81 +1,83 @@
 rankCauseSex  <- function(myLHJ="CALIFORNIA",myMeasure = "YLL",myYear=2017,myLev="lev1",myN=10,mySex="Total") {
-
   
-  if (1==2) {
-  myPlace   <- getwd()  
-  whichData <- "fake"
   
-  myLev="lev2"
-  myLHJ = "CALIFORNIA"
-  myN = 10
-  myMeasure = "YLL"
-  myYear = 2015
-  library(fs)
-  library(ggplot2)
-  library(dplyr)
-  
-   datCounty <- readRDS(path("e:","0.CBD/myCBD/","/myData/",whichData,"datCounty.RDS"))
+  if (1==1) {
+    myPlace   <- getwd()  
+    whichData <- "fake"
+    
+    myLev="lev2"
+    myLHJ = "CALIFORNIA"
+    myN = 10
+    myMeasure = "YLLper"
+    myYear = 2015
+    mySex = "Male"
+    library(fs)
+    library(ggplot2)
+    library(dplyr)
+    
+    #datCounty <- readRDS(path("e:","0.CBD/myCBD/","/myData/",whichData,"datCounty.RDS"))
+    datCounty <- readRDS(path(myPlace,"/myCBD/","/myData/",whichData,"datCounty.RDS"))
+    
   }
   
   inDat <- datCounty
   dat.1 <- filter(inDat,county==myLHJ,year==myYear,sex != "Total",Level %in% myLev,CAUSE !=0)
-  #dat.1 <- filter(inDat,county==myLHJ,year==myYear,sex != "Total",CAUSE !=0)
-  
   dat.1 <- dat.1[order( eval(parse(text=paste0("dat.1$",myMeasure)))),]
-
-   
-   
-  # ok to remove. sorting handled in ggplot
-  # if (myMeasure=="mean.age"){
-  #    dat.1 <- dat.1[order( eval(parse(text=paste0("dat.1$",myMeasure))),decreasing=TRUE),]}
   
   nR    <- nrow(dat.1)
   myNX  <- min(nR,myN) 
   dat.1 <- dat.1[((nR-myNX):nR),]
-  
   dat.1$info <- dat.1[,myMeasure]
   
-
- # https://drsimonj.svbtle.com/ordering-categories-within-ggplot2-facets
-  
-  
- # ok to remove. sorting handled in ggplot  
- # dat.1$CAUSE <- factor(dat.1$CAUSE, levels = dat.1$CAUSE[order(dat.1$info)])
-  
-  dat.1
-  
-  dat.1 <- dat.1[with(dat.1,order(info,CAUSE)),]
-  dat.1$order <- rank(factor(dat.1$info,levels = unique(dat.1$info)))
   
   dat.1$causeName <- causeList36[match(dat.1$CAUSE,causeList36[,"LABEL"]),"nameOnly"]
-   
+  
+  # https://drsimonj.svbtle.com/ordering-categories-within-ggplot2-facets
 
-  # myMeasureAlias <-        if (myMeasure=="YLL"){ "Years of Life Lost"
-  #                   } else if (myMeasure=="YLLper") { "Years of Life Lost (per 100,000 population)"
-  #                   } else if (myMeasure=="YLL.adj.rate") {"Age-Adjusted YLL Rate"
-  #                   } else if (myMeasure=="Ndeaths") {"Number of Deaths"
-  #                   } else if (myMeasure=="cDeathRate") {"Crude Death Rate"
-  #                   } else if (myMeasure=="aRate") {"Age-Adjusted Death Rate"
-  #                   } else if (myMeasure=="mean.age") {"Mean Age at Death"
-  #                   } else if (myMeasure=="SMR") {"Standard Mortality Ratio"
-  #                   }
+         if (mySex=="Female") #{dat.1       <- dat.1[with(dat.1,order(sex,-info)),]
+                              # dat.1$order <- rank(factor(dat.1$info,levels = dat.1$info))
+                              {dat.1           <- dat.1 %>%  group_by(CAUSE,add=T) %>%
+                                                  mutate(sum.info = sum(info)) %>%
+                                                  arrange(-sum.info,sex) %>%
+                                                  transform(order=match(info,unique(info)))
+  } else if (mySex=="Male")   #{dat.1 <- dat.1[with(dat.1,order(sex,info,decreasing=TRUE)),]
+                              # dat.1$order <- rank(factor(dat.1$info,levels = dat.1$info))
+                              
+                              {dat.1           <- dat.1 %>%  arrange(desc(sex),-info) %>%
+                                                  transform(order=match(info,unique(info))) %>%
+                                                  arrange(CAUSE,desc(sex)) #%>%
+                              #transform(order=match(info,unique(info)))
+                              ##    dat.1$order[duplicated(dat.1$CAUSE)] <- 
+
+  } else if (mySex=="Total")  {dat.1 <- dat.1 %>%  group_by(CAUSE,add=T) %>%
+                                        mutate(sum.info = sum(info)) %>%
+                                        arrange(-sum.info) %>%
+                                        transform(order=match(sum.info,unique(sum.info)))
+                               #dat.1$order <- factor(dat.1$order,levels = dat.1$order)
+                               }
+  
+  
+  #dat.1$order[dat.1$CAUSE==dat.1$CAUSE[!duplicated(dat.1$CAUSE)]]
+  #dat.1$order[first(dat.1$CAUSE),]
+  #rank(dat.1$order)
+  
   myMeasureAlias <-  lMeasuresC[lMeasures==myMeasure]
   
-
-  g <- ggplot(dat.1, aes(x=reorder(causeName, info),y=info,group=sex)) +
-       labs(title=paste(myMeasureAlias,"by Cause Grouped by Gender,\n","California",myYear),
-                y=paste("\n",myMeasureAlias)) +
-       facet_grid( ~ sex) + 
-       geom_col(colour = "white", fill="#428BCA") +  #alternative matching blue #0000FF
-       #scale_x_discrete(labels= paste(dat.1$CAUSE))  +
-       theme(text=element_text(size=20),
-             plot.title = element_text(colour= "black", size=20, face="bold",hjust=0.5),
-             axis.text = element_text(colour = "black", size=12),
-             axis.title.x = element_text(size=12),
-             axis.title.y = element_blank()
-             #,panel.background = element_rect(fill = "white", colour="grey50")
-                          )
+  g <- ggplot(dat.1, aes(x=order,y=info)) +
+    labs(title=paste(myMeasureAlias,"by Cause Grouped by Gender,\n","California",myYear),
+         y=paste("\n",myMeasureAlias)) +
+    facet_grid( ~ sex) +
+    scale_x_continuous(breaks = dat.1$order,labels = dat.1$causeName) +
+    geom_col(colour = "white", fill="#428BCA") +  #alternative matching blue #0000FF
+    #scale_x_discrete(labels= paste(dat.1$CAUSE))  +
+    theme(text=element_text(size=20),
+          plot.title = element_text(colour= "black", size=20, face="bold",hjust=0.5),
+          axis.text = element_text(colour = "black", size=12),
+          axis.title.x = element_text(size=12),
+          axis.title.y = element_blank()
+          ,panel.background = element_rect(fill = "white", colour="grey50")
+    )
   
   g  + coord_flip()
-
+  
 }
