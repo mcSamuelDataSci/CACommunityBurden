30aef7a2845eb90d16f8f3ca6d403b7fbecfb2ff
Hicks <Benjamin.Hicks@cdph.ca.gov>
2018-11-21 14:55:17 -0800

    updated changes

--- myCBD/myFunctions/rankCausesSex.R
+++ myCBD/myFunctions/rankCausesSex.R
@@ -33,28 +33,28 @@ rankCauseSex  <- function(myLHJ="CALIFORNIA",myMeasure = "YLL",myYear=2017,myLev
   dat.1$causeName <- causeList36[match(dat.1$CAUSE,causeList36[,"LABEL"]),"nameOnly"]
   
   # https://drsimonj.svbtle.com/ordering-categories-within-ggplot2-facets
-
-         if (mySex=="Female") #{dat.1       <- dat.1[with(dat.1,order(sex,-info)),]
-                              # dat.1$order <- rank(factor(dat.1$info,levels = dat.1$info))
-                              {dat.1           <- dat.1 %>%  group_by(CAUSE,add=T) %>%
-                                                  mutate(sum.info = sum(info)) %>%
-                                                  arrange(-sum.info,sex) %>%
-                                                  transform(order=match(info,unique(info)))
+  
+  if (mySex=="Female") #{dat.1       <- dat.1[with(dat.1,order(sex,-info)),]
+    # dat.1$order <- rank(factor(dat.1$info,levels = dat.1$info))
+  {dat.1           <- dat.1 %>%  group_by(CAUSE,add=T) %>%
+    mutate(sum.info = sum(info)) %>%
+    arrange(-sum.info,sex) %>%
+    transform(order=match(info,unique(info)))
   } else if (mySex=="Male")   #{dat.1 <- dat.1[with(dat.1,order(sex,info,decreasing=TRUE)),]
-                              # dat.1$order <- rank(factor(dat.1$info,levels = dat.1$info))
-                              
-                              {dat.1           <- dat.1 %>%  arrange(desc(sex),-info) %>%
-                                                  transform(order=match(info,unique(info))) %>%
-                                                  arrange(CAUSE,desc(sex)) #%>%
-                              #transform(order=match(info,unique(info)))
-                              ##    dat.1$order[duplicated(dat.1$CAUSE)] <- 
-
+    # dat.1$order <- rank(factor(dat.1$info,levels = dat.1$info))
+    
+  {dat.1           <- dat.1 %>%  arrange(desc(sex),-info) %>%
+    transform(order=match(info,unique(info))) %>%
+    arrange(CAUSE,desc(sex)) #%>%
+  #transform(order=match(info,unique(info)))
+  ##    dat.1$order[duplicated(dat.1$CAUSE)] <- 
+  
   } else if (mySex=="Total")  {dat.1 <- dat.1 %>%  group_by(CAUSE,add=T) %>%
-                                        mutate(sum.info = sum(info)) %>%
-                                        arrange(-sum.info) %>%
-                                        transform(order=match(sum.info,unique(sum.info)))
-                               #dat.1$order <- factor(dat.1$order,levels = dat.1$order)
-                               }
+    mutate(sum.info = sum(info)) %>%
+    arrange(-sum.info) %>%
+    transform(order=match(sum.info,unique(sum.info)))
+  #dat.1$order <- factor(dat.1$order,levels = dat.1$order)
+  }
   
   
   #dat.1$order[dat.1$CAUSE==dat.1$CAUSE[!duplicated(dat.1$CAUSE)]]
@@ -67,7 +67,7 @@ rankCauseSex  <- function(myLHJ="CALIFORNIA",myMeasure = "YLL",myYear=2017,myLev
     labs(title=paste(myMeasureAlias,"by Cause Grouped by Gender,\n","California",myYear),
          y=paste("\n",myMeasureAlias)) +
     facet_grid( ~ sex) +
-    scale_x_continuous(breaks = dat.1$order,labels = dat.1$causeName) +
+    scale_x_reverse(breaks = dat.1$order,labels = dat.1$causeName) +
     geom_col(colour = "white", fill="#428BCA") +  #alternative matching blue #0000FF
     #scale_x_discrete(labels= paste(dat.1$CAUSE))  +
     theme(text=element_text(size=20),
@@ -78,6 +78,6 @@ rankCauseSex  <- function(myLHJ="CALIFORNIA",myMeasure = "YLL",myYear=2017,myLev
           ,panel.background = element_rect(fill = "white", colour="grey50")
     )
   
-  g  + coord_flip()
+  g  + coord_flip() # scale_x_reverse()
   
 }
