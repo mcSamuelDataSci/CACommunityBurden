64a0db48c79481767947d32cce2520684a9ee988
Benjamin Hicks <benjamin.hicks@cdph.ca.gov>
2019-04-24 06:59:16 -0700

    Updated net use, poverty, education datasets. Rent still underway. Added related measures. Cleaned NAME field removal

--- myUpstream/social_determinants_of_heath/SDOHbetterMOE.R
+++ myUpstream/social_determinants_of_heath/SDOHbetterMOE.R
@@ -2,10 +2,6 @@
 myDrive <-  getwd()  
 myPlace <- paste0(myDrive,"/myCBD/")  
 upPlace <- paste0(myDrive,"/myUpstream/")  
-
-# .path   <- paste0(myDrive,"/cbd")
-# setwd(.path)
-
 .packages	  <- c("tidycensus",    #load_variables, get_acs
                  "tidyr",         #spread
                  "dplyr",         #select
@@ -16,6 +12,7 @@ lapply(.packages, require, character.only=TRUE)
 
 .ckey 	<- read_file(paste0(upPlace,"upstreamInfo/census.api.key.txt"))
 
+
 #2 User Input Variables
 #Variable Descriptions: https://www.census.gov/data/developers/data-sets.html
 ACSYear     <- 2017
@@ -24,69 +21,158 @@ Labels      <- load_variables(ACSYear,ACSSurvey) # view to see topics and labels
 #write.table(Labels, "G:/OHE/HRSU/Fusion Center/Community Burden of Disease/labels.txt", sep="\t")
 
 
+#3 Data extraction and estimate calculations
+
+# ACS Table B28002 PRESENCE AND TYPES OF INTERNET SUBSCRIPTIONS IN HOUSEHOLD
+# B28002_002 "With an Internet subscription" / B28002_001 "Total"
+# Related measures ------
+# B28003 PRESENCE OF A COMPUTER AND TYPE OF INTERNET SUBSCRIPTION IN HOUSEHOLD
+# B28004 HOUSEHOLD INCOME IN THE LAST 12 MONTHS (IN 2017 INFLATION-ADJUSTED DOLLARS) BY PRESENCE AND TYPE OF INTERNET SUBSCRIPTION IN HOUSEHOLD
+# B28005 AGE BY PRESENCE OF A COMPUTER AND TYPES OF INTERNET SUBSCRIPTION IN HOUSEHOLD
+# B28006 EDUCATIONAL ATTAINMENT BY PRESENCE OF A COMPUTER AND TYPES OF INTERNET SUBSCRIPTION IN HOUSEHOLD
+# B28007 LABOR FORCE STATUS BY PRESENCE OF A COMPUTER AND TYPES OF INTERNET SUBSCRIPTION IN HOUSEHOLD
+# B28008 PRESENCE OF A COMPUTER AND TYPE OF INTERNET SUBSCRIPTION IN HOUSEHOLD
+# B28009 PRESENCE OF A COMPUTER AND TYPE OF INTERNET SUBSCRIPTION IN HOUSEHOLD (by race, mutually exclusive)
+# ------
 acs.netuse<-get_acs(state = 06, geography = "tract", survey = ACSSurvey,
-                    year = ACSYear, variables = c("B28002_001","B28002_002"), key=.ckey,  moe_level=90)%>%  
+                    year = ACSYear, variables = c("B28002_001","B28002_002"), key=.ckey,  moe_level=90) %>%  
   gather(descriptor,value,estimate,moe) %>%
   unite(temp,variable,descriptor) %>%
   spread(temp,value) %>%
-  rename(n_net=B28002_002_estimate,N_net=B28002_001_estimate) %>%
+  rename(n_net=B28002_002_estimate,N_net=B28002_001_estimate,
+         moen_net=B28002_002_moe,moeN_net=B28002_001_moe) %>%
   mutate(est_net=n_net/N_net,
-         moe_net=moe_ratio(n_net,N_net,B28002_002_moe,B28002_001_moe),
-         B28002_002=NULL,B28002_001_moe=NULL)
+         moe_net=moe_ratio(n_net,N_net,moen_net,moeN_net)
+         ,NAME=NULL)
 
-# ACS Table B17001
-# Percent above the Federal Provery Limit
-# Related measures include median tract income
-
+# ACS Table B17001 POVERTY STATUS IN THE PAST 12 MONTHS BY SEX BY AGE (by race, mutually exclusive)
+# B17001_002 "Income in the past 12 months below poverty level" / B17001_001 "Total"
+# Related measures ------
+# B17003 POVERTY STATUS IN THE PAST 12 MONTHS OF INDIVIDUALS BY SEX BY EDUCATIONAL ATTAINMENT
+# B17004 POVERTY STATUS IN THE PAST 12 MONTHS OF INDIVIDUALS BY SEX BY WORK EXPERIENCE
+# B17005 POVERTY STATUS IN THE PAST 12 MONTHS OF INDIVIDUALS BY SEX BY EMPLOYMENT STATUS
+# B17006 POVERTY STATUS IN THE PAST 12 MONTHS OF RELATED CHILDREN UNDER 18 YEARS BY FAMILY TYPE BY AGE OF RELATED CHILDREN UNDER 18 YEARS
+# B17007 POVERTY STATUS IN THE PAST 12 MONTHS OF UNRELATED INDIVIDUALS 15 YEARS AND OVER BY SEX BY AGE
+# B17008 AGGREGATE INCOME DEFICIT (DOLLARS) IN THE PAST 12 MONTHS OF UNRELATED INDIVIDUALS BY SEX
+# B17009 POVERTY STATUS BY WORK EXPERIENCE OF UNRELATED INDIVIDUALS BY HOUSEHOLDER STATUS
+# B17010 POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES BY FAMILY TYPE BY PRESENCE OF RELATED CHILDREN UNDER 18 YEARS BY AGE OF RELATED CHILDREN
+# B17011 AGGREGATE INCOME DEFICIT (DOLLARS) IN THE PAST 12 MONTHS FOR FAMILIES BY FAMILY TYPE
+# B17012 POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES BY HOUSEHOLD TYPE BY NUMBER OF RELATED CHILDREN UNDER 18 YEARS
+# B17013 POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES BY HOUSEHOLD TYPE BY NUMBER OF PERSONS IN FAMILY
+# B17014 POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES BY HOUSEHOLD TYPE BY NUMBER OF WORKERS IN FAMILY
+# B17015 POVERTY STATUS IN THE PAST 12 MONTHS OF FAMILIES BY FAMILY TYPE BY SOCIAL SECURITY INCOME BY SUPPLEMENTAL SECURITY INCOME (SSI) AND CASH PUBLIC ASSISTANCE INCOME
+# ------
 acs.poverty<-get_acs(state = 06, geography = "tract", survey = ACSSurvey,
                      year = ACSYear, variables = c("B17001_001","B17001_002"), key=.ckey, moe_level=90) %>% 
   gather(descriptor,value,estimate,moe) %>%
   unite(temp,variable,descriptor) %>%
   spread(temp,value) %>%
-  rename(n_pov=B17001_002_estimate,N_pov=B17001_001_estimate) %>%
+  rename(n_pov=B17001_002_estimate,N_pov=B17001_001_estimate,
+         moen_pov=B17001_002_moe,moeN_pov=B17001_001_moe) %>%
   mutate(est_pov=n_pov/N_pov,
-         moe_pov=moe_ratio(n_pov,N_pov,B17001_002_moe,B17001_001_moe),
-         B17001_002_moe=NULL,B17001_001_moe=NULL)  %>%
-   select(-NAME)
+         moe_pov=moe_ratio(n_pov,N_pov,moen_pov,moeN_pov),
+         NAME=NULL)
   
-
+# ACS Table B15003 EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER (by grade level, mutually exclusive)
+# B15003_022-25 "Bachelor's degree - Doctorate degree" / B15003_001 "Total"
+# Related measures ------
+# B15001_001	SEX BY AGE BY EDUCATIONAL ATTAINMENT FOR THE POPULATION 18 YEARS AND OVER (by age grouping and grade level)
+# B15002_001	SEX BY EDUCATIONAL ATTAINMENT FOR THE POPULATION 25 YEARS AND OVER (by grade level)
+# ------
 acs.education<-get_acs(state = 06, geography = "tract", survey = ACSSurvey,
-                       year = ACSYear, variables = c("B15003_001","B15003_022"), 
+                       year = ACSYear, variables = c("B15003_001", sprintf("B15003_0%02d",22:25)), 
                        key=.ckey, moe_level=90) %>% 
-  gather(descriptor,value,estimate,moe) %>%
-  unite(temp,variable,descriptor) %>%
+  mutate(edu= ifelse( variable=="B15003_001","total","edu"),
+         NAME=NULL)%>%
+  group_by(GEOID,edu) %>%
+  #summarize(n=sum(estimate),moe=moe_sum(moe,estimate,na.rm=F)) %>%
+  summarize(n=sum(estimate),moe=moe_sum(moe[which(estimate!=0)],which(estimate!=0,arr.ind=T))) %>%  #,na.rm=F
+  gather(descriptor,value,n,moe) %>%
+  unite(temp,descriptor,edu) %>%
   spread(temp,value) %>%
-  rename(n_edu=B15003_022_estimate,N_edu=B15003_001_estimate) %>%
+  rename(N_edu=n_total,moen_edu=moe_edu,moeN_edu=moe_total) %>%
   mutate(est_edu=n_edu/N_edu,
-         moe_edu=moe_ratio(n_edu,N_edu,B15003_022_moe,B15003_001_moe),
-         moen = B15003_022_moe,moeN=B15003_001_moe) %>%
-  select(-NAME)
-
+         moe_edu=moe_ratio(n_edu,N_edu,moen_edu,moeN_edu))
 
 # raw measure is percnet in categories. Aggretated here for interpretablhy and consistany with Alameda measures.
 
+# ACS Table B25070 GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME IN THE PAST 12 MONTHS
+# B25070_007-010 "30.0 to 50.0 percent or more" / B25070_001 "Total" &
+# B25070_010     "50.0 percent or more"         / B25070_001 "Total"
+# Related measures ------
+# B25001_001	HOUSING UNITS
+# B25003_001	TENURE (by race, mutually exclusive)
+# B25007_001	TENURE BY AGE OF HOUSEHOLDER
+# B25008_001	TOTAL POPULATION IN OCCUPIED HOUSING UNITS BY TENURE
+# B25009_001	TENURE BY HOUSEHOLD SIZE
+# B25010_001	AVERAGE HOUSEHOLD SIZE OF OCCUPIED HOUSING UNITS BY TENURE
+# B25011_001	TENURE BY HOUSEHOLD TYPE (INCLUDING LIVING ALONE) AND AGE OF HOUSEHOLDER
+# B25012_001	TENURE BY FAMILIES AND PRESENCE OF OWN CHILDREN
+# B25013_001	TENURE BY EDUCATIONAL ATTAINMENT OF HOUSEHOLDER
+# B25014_001	TENURE BY OCCUPANTS PER ROOM (by race, mutually exclusive)
+# B25015_001	TENURE BY AGE OF HOUSEHOLDER BY OCCUPANTS PER ROOM
+# B25016_001	TENURE BY PLUMBING FACILITIES BY OCCUPANTS PER ROOM
+# B25020_001	TENURE BY ROOMS
+# B25021_001	MEDIAN NUMBER OF ROOMS BY TENURE
+# B25022_001	AGGREGATE NUMBER OF ROOMS BY TENURE
+# B25026_001	TOTAL POPULATION IN OCCUPIED HOUSING UNITS BY TENURE BY YEAR HOUSEHOLDER MOVED INTO UNIT
+# B25031_001	MEDIAN GROSS RENT BY BEDROOMS
+# B25032_001	TENURE BY UNITS IN STRUCTURE (by race, mutually exclusive)
+# B25033_001	TOTAL POPULATION IN OCCUPIED HOUSING UNITS BY TENURE BY UNITS IN STRUCTURE
+# B25034_001	YEAR STRUCTURE BUILT
+# B25035_001	MEDIAN YEAR STRUCTURE BUILT
+# B25036_001	TENURE BY YEAR STRUCTURE BUILT
+# B25037_001	MEDIAN YEAR STRUCTURE BUILT BY TENURE
+# B25038_001	TENURE BY YEAR HOUSEHOLDER MOVED INTO UNIT
+# B25039_001	MEDIAN YEAR HOUSEHOLDER MOVED INTO UNIT BY TENURE
+# B25042_001	TENURE BY BEDROOMS
+# B25043_001	TENURE BY TELEPHONE SERVICE AVAILABLE BY AGE OF HOUSEHOLDER
+# B25044_001	TENURE BY VEHICLES AVAILABLE
+# B25045_001	TENURE BY VEHICLES AVAILABLE BY AGE OF HOUSEHOLDER
+# B25046_001	AGGREGATE NUMBER OF VEHICLES AVAILABLE BY TENURE
+# B25049_001	TENURE BY PLUMBING FACILITIES
+# B25053_001	TENURE BY KITCHEN FACILITIES
+# B25054_001	KITCHEN FACILITIES BY MEALS INCLUDED IN RENT
+# B25055_001	AGE OF HOUSEHOLDER BY MEALS INCLUDED IN RENT
+# B25056_001	CONTRACT RENT
+# B25057_001	LOWER CONTRACT RENT QUARTILE (DOLLARS)
+# B25058_001	MEDIAN CONTRACT RENT (DOLLARS)
+# B25059_001	UPPER CONTRACT RENT QUARTILE (DOLLARS)
+# B25060_001	AGGREGATE CONTRACT RENT (DOLLARS)
+# B25061_001	RENT ASKED (by amount asked grouping)
+# B25062_001	AGGREGATE RENT ASKED (DOLLARS)
+# B25063_001	GROSS RENT
+# B25064_001	MEDIAN GROSS RENT (DOLLARS)
+# B25065_001	AGGREGATE GROSS RENT (DOLLARS)
+# B25066_001	AGGREGATE GROSS RENT (DOLLARS) BY UNITS IN STRUCTURE
+# B25067_001	AGGREGATE GROSS RENT (DOLLARS) BY MEALS INCLUDED IN RENT
+# B25068_001	BEDROOMS BY GROSS RENT
+# B25069_001	INCLUSION OF UTILITIES IN RENT
+# B25070_001	GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME IN THE PAST 12 MONTHS
+# B25071_001	MEDIAN GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME IN THE PAST 12 MONTHS (DOLLARS)
+# B25072_001	AGE OF HOUSEHOLDER BY GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME IN THE PAST 12 MONTHS
+# B25074_001	HOUSEHOLD INCOME BY GROSS RENT AS A PERCENTAGE OF HOUSEHOLD INCOME IN THE PAST 12 MONTHS
 
+# ------
 acs.rent<-get_acs(state = 06, geography = "tract", survey = ACSSurvey,
-                         year = ACSYear, variables = sprintf("B25070_0%02d",1:10),
+                         year = ACSYear, variables = c("B25070_001",(sprintf("B25070_0%02d",7:10))),
                          key=.ckey, moe_level=90) %>% 
   mutate(rent= ifelse( variable=="B25070_001",
                        "N_rent",
                ifelse( variable=="B25070_002" | variable=="B25070_003" | variable=="B25070_004" , 
-                       "rent00to19",
-               ifelse( variable=="B25070_005" | variable=="B25070_006" ,
-                       "rent20to29",
-                       "rent30up")))) %>%
-  group_by(GEOID,NAME,rent) %>%
+                       "rent30up",
+                       "rent50up")),
+         NAME=NULL) %>%
+  group_by(GEOID,rent) %>%
   #summarize(n=sum(estimate),moe=moe_sum(moe,estimate,na.rm=F)) %>%
   summarize(n=sum(estimate),moe=moe_sum(moe[which(estimate!=0)],which(estimate!=0,arr.ind=T))) %>%  #,na.rm=F
   gather(descriptor,value,n,moe) %>%
   unite(temp,descriptor,rent) %>%
   spread(temp,value) %>%
-  rename(N_rent=n_N_rent, moe_rent=moe_N_rent) %>%
+  rename(N_rent=n_N_rent, moeN_rent=moe_N_rent)# %>%
   mutate(est_rent00to19 = n_rent00to19/N_rent,
          est_rent20to29 = n_rent20to29/N_rent,
-         est_rent30up   = n_rent30up/N_rent
-         ,
+         est_rent30up   = n_rent30up/N_rent,
          moe_rent00to19 = moe_ratio(n_rent00to19, N_rent,moe_rent00to19,moe_rent),
          moe_rent20to29 = moe_ratio(n_rent20to29, N_rent,moe_rent20to29,moe_rent),
          moe_rent30up   = moe_ratio(n_rent30up  , N_rent,moe_rent30up  ,moe_rent)
