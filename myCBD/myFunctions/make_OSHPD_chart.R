6fa2ed94e08f5bbb09d4de573bb0bfcca5063e85
Carolina Downie <carolina.downie@cdph.ca.gov>
2019-05-09 14:05:13 -0700

    plotly edits/testing.

--- myCBD/myFunctions/make_OSHPD_chart.R
+++ myCBD/myFunctions/make_OSHPD_chart.R
@@ -35,22 +35,22 @@ plotly::ggplotly(plot) #This (with renderPlot in server) doesn't plot on the das
 #Other option: facet_grid(labeller=labeller(type = hospDiscMeasuresShort))--label at the end, however, this prevents wrapping of strip heading text for facets (can only do one or the other)
 
 #---------------------------------------Other plotly option----------------------------------------------------#
-oshpdPlotly <- function(myCounty = "CALIFORNIA", myOSHPDtype = "Number of Hospitalizations", mySex = "Total" ) {
+#oshpdPlotly <- function(myCounty = "CALIFORNIA", myOSHPDtype = "Number of Hospitalizations", mySex = "Total" ) {
   
-  plotData <-    calculated_metrics %>% 
-    mutate(type = factor(type, levels = c("n_hosp", "cHospRate", "ahospRate", "charges", "cChargeRate", "avgcharge"))) %>% 
-    mutate(type = plyr::revalue(type, hospDiscMeasures)) %>% #replaces values with full name labels
-    left_join(., fullCauseList, by = c("CAUSE" = "LABEL")) %>% 
-    filter(!is.na(CAUSE), Level == "lev2", county == myCounty, !(type %in% c("Crude Hosp Rate","Crude Charge Rate"))) %>% 
-    filter(sex == mySex) %>%
-    group_by(type) %>% 
-    mutate(nameOnly = forcats::fct_reorder(nameOnly, filter(., type == myOSHPDtype) %>% 
-                                             pull(measure)))  
-  
-  plotly::plot_ly(plotData, x = ~nameOnly, y = ~measure, orientation = "h") %>% add_bars() 
-    
-    #facet_grid(. ~ type, scales = "free_x", labeller=labeller(type = label_wrap_gen(5))) + 
-    #theme_bw() + #need to specify theme first, before changing/removing axis titles/labels etc. If theme_bw() is put at end, it negates all of these changes
+  # #plotData <-    calculated_metrics %>% 
+  #   #mutate(type = factor(type, levels = c("n_hosp", "cHospRate", "ahospRate", "charges", "cChargeRate", "avgcharge"))) %>% 
+  #   #mutate(type = plyr::revalue(type, hospDiscMeasures)) %>% #replaces values with full name labels
+  #   #left_join(., fullCauseList, by = c("CAUSE" = "LABEL")) %>% 
+  #   filter(!is.na(CAUSE), Level == "lev2", county == myCounty, !(type %in% c("Crude Hosp Rate","Crude Charge Rate"))) %>% 
+  #   filter(sex == mySex) %>%
+  #   group_by(type) %>% 
+  #   mutate(nameOnly = forcats::fct_reorder(nameOnly, filter(., type == myOSHPDtype) %>% 
+  #                                            pull(measure)))  
+  # 
+  # plotly::plot_ly(plotData, x = ~nameOnly, y = ~measure, orientation = "h") %>% add_bars() 
+  #   
+  #   #facet_grid(. ~ type, scales = "free_x", labeller=labeller(type = label_wrap_gen(5))) + 
+  #   #theme_bw() + #need to specify theme first, before changing/removing axis titles/labels etc. If theme_bw() is put at end, it negates all of these changes
     #scale_y_continuous(labels = scales::comma) + #numbers shown with commas rather than scientific notation
     #scale_x_discrete(labels = scales::wrap_format(10)) + #x-axis is condition label--wrapping text so it stacks on top of each other
     #within theme--x and y axis refer to the way it looks, with coord_flip(), so y refers to vertical label (which technically is really x axis) and vice versa with x axis
