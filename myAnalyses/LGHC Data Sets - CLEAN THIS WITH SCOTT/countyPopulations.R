647340c541309a6c63e96a39628d6a5ae9b2b0e1
Michael CDPH <samfam921@gmail.com>
2020-11-13 21:25:00 -0800

    delete and clean (desktop version)

--- myAnalyses/LGHC Data Sets - CLEAN THIS WITH SCOTT/countyPopulations.R
+++
@@ -1,7 +0,0 @@
-popWork <- popCounty %>% 
-              filter(sex=="Total") %>%
-              group_by(year,county) %>%
-              summarise(pop=sum(pop))
-
-
-pop2017 <- filter(popWork,year==2017)
