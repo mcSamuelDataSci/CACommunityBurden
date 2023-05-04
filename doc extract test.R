a0fc883bf4133f03a748b811e4c70aa519d66105
kangjaspreet <jaspreetkang@ucla.edu>
2023-05-03 17:10:45 -0700

    Remove junk files in root

--- doc extract test.R
+++
@@ -1,8 +0,0 @@
-server <- TRUE
-if (server) highPath <-  "/mnt/projects/"
-if(!server) highPath <-  "G:/"
-
-
-library(docxtractr)
-docX <- read_docx(paste0(highPath,"FusionData/State of Public Health/VizText.docx"))
-ourText <- docx_extract_tbl(docX, 1)
