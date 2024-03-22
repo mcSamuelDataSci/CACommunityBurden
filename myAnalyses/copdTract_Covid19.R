c5972ddd9195b9b1962db3cce9b6ff23b446ff12
kangjaspreet <jaspreetkang@ucla.edu>
2024-03-22 16:34:02 -0700

    Ignore Analyses

--- myAnalyses/copdTract_Covid19.R
+++
@@ -1,8 +0,0 @@
-
-
-copdPlusTract <- read_csv("F:/0.CBD/myCBD/myData/real/analysisDataSets/Tract.csv")  %>%
-                    filter(CAUSE %in% c("0","D66", "A07", "D68"),sex=="Total") %>%
-                    select(-oDeaths,-YLL.adj.rate)
-
-
-write_csv(copdPlusTract,"copdPlusTract.csv")
